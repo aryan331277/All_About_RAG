939f5b8b55dfa79fcd1d4772fbc130ec383464c8
aryan <bt24csd009@iiitn.ac.in>
2025-12-10 07:10:48 +0530

    Update bm25_lib.py

--- embeddings/bm25/bm25_lib.py
+++ embeddings/bm25/bm25_lib.py
@@ -2,8 +2,8 @@ from rank_bm25 import BM25Okapi
 from nltk.tokenize import word_tokenize#tokeniser
 import re
 
-docs=[#define document here]
-tokenized=[word_tokenize(doc.lower()) for doc in docs]#tokenised documents
+chunks=[#define document here]
+tokenized=[word_tokenize(doc.lower()) for doc in chunks]#tokenised documents
 
 bm25=BM25Okapi(tokenized)
 
@@ -11,7 +11,7 @@ query=""#define the query here
 query_tokenized=word_tokenize(query.lower())
 scores=bm25.get_scores(query_tokenized)
 
-sorted_docs=sorted(zip(scores,docs),key=lambda x: -x[0])
+sorted_chunks=sorted(zip(scores,chunks),key=lambda x: -x[0])
 
-for rank,(score,doc) in enumerate(sorted_docs,start=1):
+for rank,(score,doc) in enumerate(sorted_chunks,start=1):
   print(f"Rnak: {rank} (score={score:.4f}): {doc}")
