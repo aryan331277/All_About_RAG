371af0bfd86a4ce552e45bcf4cd76ad2e19290b1
aryan <bt24csd009@iiitn.ac.in>
2025-12-10 07:10:11 +0530

    Refactor BM25 to use chunks instead of docs

--- embeddings/bm25/bm25_dense_embeddings.py
+++ embeddings/bm25/bm25_dense_embeddings.py
@@ -2,11 +2,11 @@ from rank_bm25 import BM25Okapi
 from nltk.tokenize import word_tokenize#tokeniser
 import re
 
-docs=[#define document here]
-tokenized=[word_tokenize(doc.lower()) for doc in docs]#tokenised documents
+chunks=[#define chunk here]
+tokenized=[word_tokenize(chunk.lower()) for chunk in chunks]#tokenised chunk
 
 model=SentenceTransformer("all-MiniLM-L6-v2")
-doc_embeds=model.encode(docs,normalize_embeddings=True)
+chunk_embeds=model.encode(chunks,normalize_embeddings=True)
 
 query = ""  # define query
 query_tokenized=word_tokenize(query.lower())
@@ -14,7 +14,7 @@ query_tokenized=word_tokenize(query.lower())
 bm25_scores=np.array(bm25.get_scores(query_tokenized))
 
 query_embed=model.encode([query], normalize_embeddings=True)[0]
-dense_scores=doc_embeds @ query_embed
+dense_scores=chunk_embeds @ query_embed
 
 
 alpha=0.5  
@@ -26,10 +26,10 @@ else:
 hybrid_scores=alpha * bm25_norm +(1 - alpha) * dense_scores
 
 
-sorted_docs = sorted(zip(hybrid_scores,docs),key=lambda x: -x[0])
+sorted_chunks = sorted(zip(hybrid_scores,chunks),key=lambda x: -x[0])
 
-for rank,(score,doc) in enumerate(sorted_docs,start=1):
-  print(f"Rnak: {rank} (score={score:.4f}): {doc}")
+for rank,(score,chunk) in enumerate(sorted_chunks,start=1):
+  print(f"Rnak: {rank} (score={score:.4f}): {chunk}")
 
 
 
