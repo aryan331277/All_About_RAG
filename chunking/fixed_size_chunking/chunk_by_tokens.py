ee75a973ea7927c9b778b76c1c5a95a57aec231d
aryan <bt24csd009@iiitn.ac.in>
2025-12-11 11:41:04 +0530

    Fix indentation and return statement in chunk_by_tokens

--- chunking/fixed_size_chunking/chunk_by_tokens.py
+++ chunking/fixed_size_chunking/chunk_by_tokens.py
@@ -6,6 +6,7 @@ def chunk_by_tokens(text,token_size):
   for i in range(0,len(tokens), token_size):
     chunk= ' '.join(tokens[i:i + token_size])
     chunks.append(chunk)
+  return chunks
 
 #calling the function
 result = chunk_by_tokens(docs,50)
