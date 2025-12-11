e19109044092906715bc1b3c05c12c42aa41f841
aryan <bt24csd009@iiitn.ac.in>
2025-12-11 09:49:26 +0530

    Update vector computation in splade.py

--- embeddings/sparse/splade/splade.py
+++ embeddings/sparse/splade/splade.py
@@ -9,10 +9,14 @@ def splade(name):
     with torch.no_grad():
       reps = model(**inputs)
       d_rep = reps.d_rep if hasattr(reps,"d_rep") else reps.last_hidden_state.mean(1)
-      vec = torch.max(vec,dim=0)
-
-
+      vec = torch.log1p(torch.relu(d_rep))
+      vec = torch.max(vec,dim=0).values
+      indices=vec.nonzero(as_tuple=True)
+      values= vec[indices].numpy()
+      indices= indices.numpy()
+    return (zip(map(int, indices), map(float, values)))
+  return encode
 
 splade_ensemble = splade("naver/splade-cocondenser-ensembledistil")      # SPLADE-Ensemble Distil
-splade_pp = get_splade("naver/splade-v3")                           # Splade++
-distil_splade = get_splade("naver/splade-v3-distil")                # Distil-Splade
+splade_pp = splade("naver/splade-v3")                           # Splade++
+distil_splade = splade("naver/splade-v3-distil")                # Distil-Splade
